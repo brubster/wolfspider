f146d77b1c61a20e3c6581756c0b38ec20206949
Evan <evan.c.ohme@gmail.com>
2025-05-20 13:54:14 -0700

    Only update mesh position on _physics_process

--- src/player/mesh.gd
+++ src/player/mesh.gd
@@ -2,6 +2,6 @@ extends MeshInstance3D
 
 @export var collider: CollisionShape3D
 
-func _process(_delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	self.position.y = collider.position.y
 	self.mesh.height = collider.shape.height
